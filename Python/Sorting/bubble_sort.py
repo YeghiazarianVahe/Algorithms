9c7707d7d335e6bc291b23c124ce8e87377e8978
Vahe Yeghiazaryan <yeghiazarian.vahe@gmail.com>
2024-07-04 12:21:08 +0400

    Modified and optimized bubble_sort algorithm

--- Python/Sorting/bubble_sort.py
+++ Python/Sorting/bubble_sort.py
@@ -3,7 +3,12 @@ from typing import List
 
 def bubble_sort(vector: List[int]) -> List[int]:
     for i in range(len(vector)):
-        for j in range(len(vector) - 1):
+        flag: bool = False
+        for j in range(len(vector) - 1 - i):
             if vector[j+1] < vector[j]:
+                flag = True
                 vector[j+1], vector[j] = vector[j], vector[j+1]
+
+        if not flag:
+            break
     return vector
